6ca3f379d4459747cc29b10cbe2ef7d31c93f059
brusk <brusk@5d1ad3b6-ec95-8a4f-9091-d39b7dd61df8>
2011-07-22 12:57:43 +0000

    git-svn-id: svn://geyce-rep01/jEa@5619 5d1ad3b6-ec95-8a4f-9091-d39b7dd61df8

--- mae/easp/general/ErrorParamJ.java
+++ mae/easp/general/ErrorParamJ.java
@@ -11,23 +11,23 @@ public class ErrorParamJ extends Hashtable{
 			if (param != null){
 				put(ErrorManager.APLICATION, param.getAplication().getName());
 				Program prog = param.getProgram();
-				if (prog != null){
+				if (prog != null && prog.getName()!=null){
 					put(ErrorManager.PROGRAM, prog.getName());
 				}
 				Form form = param.getForm();
-				if (form != null) {
+				if (form != null && form.getName()!=null) {
 					put(ErrorManager.FORM, form.getName());
 				}
 		        VisualComponent ctrl = param.getControl();
-		        if (ctrl != null){
+		        if (ctrl != null && ctrl.getName()!=null){
 		        	put(ErrorManager.CONTROL, ctrl.getName());
 		        }
 		        Report report = param.getReport();
-		        if (report != null){
+		        if (report != null && report.getName()!=null){
 		        	put(ErrorManager.REPORT, report.getName());
 		        }
 		        DataBase db = param.getDB();
-		        if (db!=null){
+		        if (db!=null && db.getName()!=null){
 		        	put(ErrorManager.DATABASE, db.getName());
 		        }
 		        String mens = param.getMessage();
