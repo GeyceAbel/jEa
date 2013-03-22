d4488956d6d7eec31b3907c2fb483b6a750779d9
xavi <xavi@5d1ad3b6-ec95-8a4f-9091-d39b7dd61df8>
2013-03-22 10:52:55 +0000

    CLASES JASPER
    git-svn-id: svn://geyce-rep01/jEa@10351 5d1ad3b6-ec95-8a4f-9091-d39b7dd61df8

--- montajea.bat
+++ montajea.bat
@@ -1,7 +1,7 @@
 @echo off
 REM  **********  Aqui se especifica la version , y los paquetes de jtoken que se van a crear ***********
-set versio=130303
-set version=13.03.03
+set versio=130305
+set version=13.03.05
 
 GOTO JEA
 :FIJEA
