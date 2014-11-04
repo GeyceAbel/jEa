c36c55251db7e4bb1188a833d12ccce7f1084c19
brusk <brusk@5d1ad3b6-ec95-8a4f-9091-d39b7dd61df8>
2014-11-04 11:52:49 +0000

    git-svn-id: svn://geyce-rep01/jEa@14811 5d1ad3b6-ec95-8a4f-9091-d39b7dd61df8

--- montajea.bat
+++ montajea.bat
@@ -1,8 +1,8 @@
 @echo off
 REM  **********  Aqui se especifica la version , y los paquetes de jtoken que se van a crear ***********
 
-set versio=141003
-set version=14.10.03
+set versio=141004
+set version=14.10.04
 
 GOTO JEA
 :FIJEA
