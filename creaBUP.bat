4f62a0571a7690bf003b3bb4c773ac92e0db010a
brusk <brusk@5d1ad3b6-ec95-8a4f-9091-d39b7dd61df8>
2014-11-11 14:47:00 +0000

    git-svn-id: svn://geyce-rep01/jEa@14863 5d1ad3b6-ec95-8a4f-9091-d39b7dd61df8

--- creaBUP.bat
+++ creaBUP.bat
@@ -3,35 +3,35 @@ if "%1" == "" goto misatge
 if "%2" == "" goto misatge
 if "%3" == "" goto misatge
 set versio=%1%2%3
-md c:\Backup\jEa
-del c:\Backup\jEa\copia_jEa_%versio%.zip
+md e:\Backup\jEa
+del e:\Backup\jEa\copia_jEa_%versio%.zip
 
-pkzip25 -add -direct  c:\Backup\jEa\copia_jEa_%versio%.zip C:\Projectes\jEa\*.html
-pkzip25 -add -direct  c:\Backup\jEa\copia_jEa_%versio%.zip C:\Projectes\jEa\*.jpg
-pkzip25 -add -direct  c:\Backup\jEa\copia_jEa_%versio%.zip C:\Projectes\jEa\*.gif
-pkzip25 -add -direct  c:\Backup\jEa\copia_jEa_%versio%.zip C:\Projectes\jEa\*.xml
-pkzip25 -add -direct  c:\Backup\jEa\copia_jEa_%versio%.zip C:\Projectes\jEa\*.xsl
-pkzip25 -add -direct  c:\Backup\jEa\copia_jEa_%versio%.zip C:\Projectes\jEa\*.xls
-pkzip25 -add -direct  c:\Backup\jEa\copia_jEa_%versio%.zip C:\Projectes\jEa\*.vbs
-pkzip25 -add -direct  c:\Backup\jEa\copia_jEa_%versio%.zip C:\Projectes\jEa\*.bat
-pkzip25 -add -direct  c:\Backup\jEa\copia_jEa_%versio%.zip C:\Projectes\jEa\*.properties
-pkzip25 -add -direct  c:\Backup\jEa\copia_jEa_%versio%.zip C:\Projectes\jEa\*.dll
-pkzip25 -add -direct  c:\Backup\jEa\copia_jEa_%versio%.zip C:\Projectes\jEa\*.create
-pkzip25 -add -direct  c:\Backup\jEa\copia_jEa_%versio%.zip C:\Projectes\jEa\*.alter
-pkzip25 -add -direct  c:\Backup\jEa\copia_jEa_%versio%.zip C:\Projectes\jEa\*.java
+pkzip25 -add -direct  e:\Backup\jEa\copia_jEa_%versio%.zip e:\Projectes\jEa\*.html
+pkzip25 -add -direct  e:\Backup\jEa\copia_jEa_%versio%.zip e:\Projectes\jEa\*.jpg
+pkzip25 -add -direct  e:\Backup\jEa\copia_jEa_%versio%.zip e:\Projectes\jEa\*.gif
+pkzip25 -add -direct  e:\Backup\jEa\copia_jEa_%versio%.zip e:\Projectes\jEa\*.xml
+pkzip25 -add -direct  e:\Backup\jEa\copia_jEa_%versio%.zip e:\Projectes\jEa\*.xsl
+pkzip25 -add -direct  e:\Backup\jEa\copia_jEa_%versio%.zip e:\Projectes\jEa\*.xls
+pkzip25 -add -direct  e:\Backup\jEa\copia_jEa_%versio%.zip e:\Projectes\jEa\*.vbs
+pkzip25 -add -direct  e:\Backup\jEa\copia_jEa_%versio%.zip e:\Projectes\jEa\*.bat
+pkzip25 -add -direct  e:\Backup\jEa\copia_jEa_%versio%.zip e:\Projectes\jEa\*.properties
+pkzip25 -add -direct  e:\Backup\jEa\copia_jEa_%versio%.zip e:\Projectes\jEa\*.dll
+pkzip25 -add -direct  e:\Backup\jEa\copia_jEa_%versio%.zip e:\Projectes\jEa\*.create
+pkzip25 -add -direct  e:\Backup\jEa\copia_jEa_%versio%.zip e:\Projectes\jEa\*.alter
+pkzip25 -add -direct  e:\Backup\jEa\copia_jEa_%versio%.zip e:\Projectes\jEa\*.java
 
-copy \\geyce-srv01\amesp\Projectes\Easp\Ver-4.9\Desenvolupament\jedicase.mdb c:\Backup\jEa
-copy \\geyce-srv01\amesp\Projectes\Easp\Ver-4.9\Desenvolupament\*.sql c:\Backup\jEa
-copy \\geyce-srv01\amesp\Projectes\Easp\Ver-4.9\Desenvolupament\*.bat c:\Backup\jEa
+copy \\geyce-srv01\amesp\Projectes\Easp\Ver-4.9\Desenvolupament\jedicase.mdb e:\Backup\jEa
+copy \\geyce-srv01\amesp\Projectes\Easp\Ver-4.9\Desenvolupament\*.sql e:\Backup\jEa
+copy \\geyce-srv01\amesp\Projectes\Easp\Ver-4.9\Desenvolupament\*.bat e:\Backup\jEa
 
-pkzip25 -add   c:\Backup\jEa\copia_jEa_%versio%.zip c:\backup\jEa\jedicase.mdb
-pkzip25 -add   c:\Backup\jEa\copia_jEa_%versio%.zip c:\backup\jEa\*.sql
-pkzip25 -add   c:\Backup\jEa\copia_jEa_%versio%.zip c:\backup\jEa\*.bat
+pkzip25 -add   e:\Backup\jEa\copia_jEa_%versio%.zip e:\backup\jEa\jedicase.mdb
+pkzip25 -add   e:\Backup\jEa\copia_jEa_%versio%.zip e:\backup\jEa\*.sql
+pkzip25 -add   e:\Backup\jEa\copia_jEa_%versio%.zip e:\backup\jEa\*.bat
 
-del c:\backup\jEa\jedicase.mdb
-del c:\backup\jEa\*.sql
-del c:\backup\jEa\*.bat
-copy c:\Backup\jEa\copia_jEa_%versio%.zip \\geyce-srv01\amesp\copies\jordi\jEa\copia_jEa_%versio%.zip
+del e:\backup\jEa\jedicase.mdb
+del e:\backup\jEa\*.sql
+del e:\backup\jEa\*.bat
+copy e:\Backup\jEa\copia_jEa_%versio%.zip \\geyce-srv01\amesp\copies\jordi\jEa\copia_jEa_%versio%.zip
 
 goto final
 :misatge
