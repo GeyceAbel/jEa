dd81707ada6aea5ad0a54a26696e7c133fd30c03
xavi <xavi@5d1ad3b6-ec95-8a4f-9091-d39b7dd61df8>
2014-12-05 09:57:32 +0000

    Compilacio ultims canvis
    git-svn-id: svn://geyce-rep01/jEa@15020 5d1ad3b6-ec95-8a4f-9091-d39b7dd61df8

--- montajea.bat
+++ montajea.bat
@@ -1,8 +1,8 @@
 @echo off
 REM  **********  Aqui se especifica la version , y los paquetes de jtoken que se van a crear ***********
 
-set versio=141201
-set version=14.12.01
+set versio=141202
+set version=14.12.02
 
 GOTO JEA
 :FIJEA
