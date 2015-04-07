32bd2f329a37eb3647454eee7a3c456dca4f649a
pere <pere@5d1ad3b6-ec95-8a4f-9091-d39b7dd61df8>
2015-04-07 11:51:52 +0000

    git-svn-id: svn://geyce-rep01/jEa@16071 5d1ad3b6-ec95-8a4f-9091-d39b7dd61df8

--- montajea.bat
+++ montajea.bat
@@ -1,8 +1,8 @@
 @echo off
 REM  **********  Aqui se especifica la version , y los paquetes de jtoken que se van a crear ***********
 
-set versio=150401
-set version=15.04.01
+set versio=150402
+set version=15.04.02
 
 GOTO JEA
 :FIJEA
