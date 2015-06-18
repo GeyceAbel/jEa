79e9eba9c9503401547d54ed1cbd526ca797c0eb
xavi <xavi@5d1ad3b6-ec95-8a4f-9091-d39b7dd61df8>
2015-06-18 15:54:46 +0000

    Conversio logic class conjunta. Opcio X traspas nivell convenio jNomina
    git-svn-id: svn://geyce-rep01/jEa@16806 5d1ad3b6-ec95-8a4f-9091-d39b7dd61df8

--- mae/easp/cnvlogicclass/ProgCnvlogicclass.java
+++ mae/easp/cnvlogicclass/ProgCnvlogicclass.java
@@ -1,6 +1,6 @@
 // Codigo Generado por MAEFCASE V-4.0 NO MODIFICAR!
-// Fecha:            20150609
-// Hora:             09:17:34
+// Fecha:            20150618
+// Hora:             17:54:03
 // Driver BD:        ODBC
 // Base de Datos:    bdeaspprog
 // 
@@ -926,13 +926,13 @@ public class ProgCnvlogicclass extends Program
         setType(STRING);
         setLength(1);
         setPrintable(false);
-        setRestricted(false);
+        addItem("X/Convenio");
         addItem("E/Empresa");
-        addItem("C/Convenio");
+        addItem("C/Conv-Emp");
         }
       public Object getDefault()
         {
-        return new String("E");
+        return new String("X");
         }
       public boolean obligate()
         {
