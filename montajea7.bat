10be416741ec2bbe230988c396e66e2942c09795
jgili <jgili@5d1ad3b6-ec95-8a4f-9091-d39b7dd61df8>
2018-10-26 08:47:08 +0000

    git-svn-id: svn://geyce-rep01/jEa@28521 5d1ad3b6-ec95-8a4f-9091-d39b7dd61df8

--- montajea7.bat
+++ montajea7.bat
@@ -1,8 +1,8 @@
 @echo off
 REM  **********  Aqui se especifica la version , y los paquetes de jtoken que se van a crear ***********
 
-set versio=_7_181002
-set version=18.10.02
+set versio=_7_181003
+set version=18.10.03
 
 GOTO JEA
 :FIJEA
