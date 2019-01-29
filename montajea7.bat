7db27c674a9eea759050b321b6caf28f059d1b88
pau <pau@5d1ad3b6-ec95-8a4f-9091-d39b7dd61df8>
2019-01-29 10:50:45 +0000

    Nuevo paquete de jEA con la eliminación del Catalogo ADMON que siempre habia sido igual al de EASP ,pero antes apuntaba al mismo fichero y era facil mantenerlos los 2 ahora como con el jedicase nuevo son tablas , se tenia de insertar las modificaciones en los 2 catalogos y esto era un trabajo extra aparentemente innecesario.
    git-svn-id: svn://geyce-rep01/jEa@29290 5d1ad3b6-ec95-8a4f-9091-d39b7dd61df8

--- montajea7.bat
+++ montajea7.bat
@@ -1,8 +1,8 @@
 @echo off
 REM  **********  Aqui se especifica la version , y los paquetes de jtoken que se van a crear ***********
 
-set versio=_7_190102
-set version=19.01.02
+set versio=_7_190103
+set version=19.01.03
 
 GOTO JEA
 :FIJEA
