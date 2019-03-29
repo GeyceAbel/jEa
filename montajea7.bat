5bcfc73138dda03dd113fe5d2037c219b82bb41f
brusk <brusk@5d1ad3b6-ec95-8a4f-9091-d39b7dd61df8>
2019-03-29 09:22:24 +0000

    git-svn-id: svn://geyce-rep01/jEa@29893 5d1ad3b6-ec95-8a4f-9091-d39b7dd61df8

--- montajea7.bat
+++ montajea7.bat
@@ -1,8 +1,8 @@
 @echo off
 REM  **********  Aqui se especifica la version , y los paquetes de jtoken que se van a crear ***********
 
-set versio=_7_190303
-set version=19.03.03
+set versio=_7_190304
+set version=19.03.04
 
 GOTO JEA
 :FIJEA
