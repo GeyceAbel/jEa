a0adb424f1388ffc0bca233bfc08e974711b74bc
Ester <Ester@5d1ad3b6-ec95-8a4f-9091-d39b7dd61df8>
2019-09-18 09:19:09 +0000

    git-svn-id: svn://geyce-rep01/jEa@31375 5d1ad3b6-ec95-8a4f-9091-d39b7dd61df8

--- montajea7.bat
+++ montajea7.bat
@@ -1,8 +1,8 @@
 @echo off
 REM  **********  Aqui se especifica la version , y los paquetes de jtoken que se van a crear ***********
 
-set versio=_7_190901
-set version=19.09.01
+set versio=_7_190902
+set version=19.09.02
 
 GOTO JEA
 :FIJEA
