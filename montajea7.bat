435d3b5d4a93d078a4237173e3549c5556973619
pere <pere@5d1ad3b6-ec95-8a4f-9091-d39b7dd61df8>
2020-03-09 10:32:55 +0000

    git-svn-id: svn://geyce-rep01/jEa@33173 5d1ad3b6-ec95-8a4f-9091-d39b7dd61df8

--- montajea7.bat
+++ montajea7.bat
@@ -1,8 +1,8 @@
 @echo off
 REM  **********  Aqui se especifica la version , y los paquetes de jtoken que se van a crear ***********
 
-set versio=_7_200301
-set version=20.03.01
+set versio=_7_200302
+set version=20.03.02
 
 GOTO JEA
 :FIJEA
