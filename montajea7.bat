0f698d252ce256be55b50ee87f6fd078ea198bfd
pau <pau@5d1ad3b6-ec95-8a4f-9091-d39b7dd61df8>
2022-06-22 11:33:19 +0000

    build monta i nou zip .
    git-svn-id: svn://geyce-rep01/jEa@43228 5d1ad3b6-ec95-8a4f-9091-d39b7dd61df8

--- montajea7.bat
+++ montajea7.bat
@@ -1,8 +1,8 @@
 @echo off
 REM  **********  Aqui se especifica la version , y los paquetes de jtoken que se van a crear ***********
 
-set versio=_7_220605
-set version=22.06.05
+set versio=_7_220606
+set version=22.06.06
 
 GOTO JEA
 :FIJEA
